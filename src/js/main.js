08f10ddeeea70263bf79efb9282a077c973f4df1
rxpro <rpro700@mail.ru>
2024-03-24 19:21:21 +0300

    Add: the project with 8 adaptive pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,28 @@
 import $ from 'jquery';
-
+
 import './polyfills';
-
+
 import Preload from './components/preload';
-
+
 import Fancybox from './components/fancybox';
-
+
 import LazyLoad from './components/lazyLoad';
-
+
 import Swiper from './components/swiper';
-
+
 import Tooltip from './components/tooltip';
-
+
 import Phone from './components/phone';
-
+
 import Select from './components/select';
-
+
 import Accordion from './components/accordion';
-
+
 import Tabs from './components/tabs';
 
 window.$ = window.jQuery = $;
 
-$(function() {
+$(function () {
     new Preload('[data-preload]');
 
     new Fancybox('[data-fancybox]');
@@ -51,4 +51,11 @@ $(function() {
     new Accordion('[data-accordion]');
 
     new Tabs('[data-tabs]');
+
+    $('.header__burger').on('click', function (e) {
+        e.preventDefault();
+
+        $('.header').toggleClass('is-active');
+        $('body').toggleClass('no-scroll');
+    });
 });
